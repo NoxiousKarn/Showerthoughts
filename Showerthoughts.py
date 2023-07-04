cbaf0ce0a4b9060dcf18d75f44c32d9e8285cd47
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-04 05:25:22 -0500

    Create Showerthoughts.py

---
+++ Showerthoughts.py
@@ -0,0 +1,54 @@
+import subprocess
+import re
+import random
+import os
+import pwnagotchi.plugins as plugins
+
+
+class ShowerThoughtsPlugin(plugins.Plugin):
+    __author__ = 'Noxiouskarn & GPT3'
+    __version__ = '1.0.0'
+    __license__ = 'GPL3'
+    __description__ = 'Display random shower thoughts rss headlines when waiting. '
+
+    def __init__(self):
+        self.rss_url = self.options['rss_url']
+        self.saved_rss = self.options['saved_rss']
+        self.max_title_length = self.options['max_title_length']
+        self.download_rss_feed()
+
+    def on_wait(self, agent, seconds):
+        headline = self.get_random_headline()
+        agent.display.write(headline)  # Update the Pwnagotchi display with the headline
+
+    def download_rss_feed(self):
+        command = "curl --silent {} --user-agent 'Mozilla' --output {}".format(self.rss_url, self.saved_rss)
+        subprocess.run(command, shell=True)
+
+    def remove_long_titles(self):
+        with open(self.saved_rss, 'r') as f:
+            data = f.read()
+
+        title_regex = r'<title>(.*?)</title>'
+        long_titles = re.findall(title_regex, data)
+
+        for title in long_titles:
+            if len(title) > self.max_title_length:
+                data = data.replace("<title>{}</title>".format(title), '')
+
+        with open(self.saved_rss, 'w') as f:
+            f.write(re.sub(title_regex, '', data))
+
+    def get_random_headline(self):
+        self.remove_long_titles()
+
+        with open(self.saved_rss, 'r') as f:
+            data = f.read()
+
+        title_regex = r'<title>(.*?)</title>'
+        headlines = re.findall(title_regex, data)
+
+        return random.choice(headlines)
+
+
+plugins.register(ShowerThoughtsPlugin())
