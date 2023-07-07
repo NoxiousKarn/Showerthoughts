1b00826dcbf49aed930510bc365164d97467b7f7
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-07 01:58:10 -0500

    remove_long_titles.py
    Added Logging info
    Added code comments

--- remove_long_titles.py
+++ remove_long_titles.py
@@ -2,19 +2,31 @@ import re
 import os
 
 def remove_long_titles(filename):
+    # This function removes long headlines from an RSS feed.
+
+    # Open the RSS feed file in read mode.
     with open(filename, 'r') as f:
         data = f.read()
 
+    # Use a regular expression to extract all the headlines from the file.
     title_regex = r'<title>(.*?)</title>'
     headlines = re.findall(title_regex, data)
 
+    # Iterate over the headlines and remove any headlines that are longer than 68 characters or that are the word "showerthoughts".
     for headline in headlines:
         if len(headline) > 68 or headline.strip().lower() == "showerthoughts":
             data = data.replace("<title>{}</title>".format(headline), '')
 
+    # Write the modified data to the file.
     with open(filename, 'w') as f:
         f.write(data)
 
 if __name__ == '__main__':
+    # Get the filename of the RSS feed.
     filename = '/root/showerthoughts.rss'
+
+    # Remove long headlines from the file.
     remove_long_titles(filename)
+
+    # Log the fact that the function has finished running.
+    logging.info('Finished removing long headlines from RSS feed.')
