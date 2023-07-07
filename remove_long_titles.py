2556023e9bc03b7314c7482d0031dc8e334b58a0
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-07 04:14:36 -0500

    remove_long_titles.py

--- remove_long_titles.py
+++ remove_long_titles.py
@@ -10,15 +10,18 @@ def remove_long_titles(filename):
     title_regex = r'<title>(.*?)</title>'
     headlines = re.findall(title_regex, data)
 
+    short_headlines = []
     for headline in headlines:
-        if len(headline) > 68 or headline.strip().lower() == "showerthoughts":
-            data = data.replace("<title>{}</title>".format(headline), '')
+        if len(headline) <= 68 and headline.strip().lower() != "showerthoughts":
+            short_headlines.append(headline)
 
     with open(filename, 'w') as f:
         f.write(data)
 
     with open('/root/shortshowerthoughts.rss', 'a') as f:
-        f.write(data)
+        for headline in short_headlines:
+            if headline not in f.read():
+                f.write('<title>{}</title>'.format(headline))
 
 if __name__ == '__main__':
     filename = '/root/showerthoughts.rss'
@@ -26,4 +29,3 @@ if __name__ == '__main__':
     remove_long_titles(filename)
 
     logging.info('Finished removing long headlines from RSS feed.')
-
