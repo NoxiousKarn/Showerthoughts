7757d4ac261a1b83cf7a9cffda787ad1126bc318
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-07 04:35:52 -0500

    remove_long_titles.py
    Added code to remove duplicates from shortshowerthoughts.rss after writing headlines less than 68 characters in length ti the file.

--- remove_long_titles.py
+++ remove_long_titles.py
@@ -3,15 +3,12 @@ import os
 import logging
 
 def remove_long_titles(filename):
-    
 
     with open(filename, 'r') as f:
         data = f.read()
 
-
     title_regex = r'<title>(.*?)</title>'
     headlines = re.findall(title_regex, data)
-
 
     for headline in headlines:
         if len(headline) > 68 or headline.strip().lower() == "showerthoughts":
@@ -23,9 +20,25 @@ def remove_long_titles(filename):
     with open('/root/shortshowerthoughts.rss', 'a') as f:
         f.write(data)
 
+def check_for_duplicates(filename):
+
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    title_regex = r'<title>(.*?)</title>'
+    headlines = set(re.findall(title_regex, data))
+
+    for headline in headlines:
+        if data.count("<title>{}</title>".format(headline)) > 1:
+            data = data.replace("<title>{}</title>".format(headline), '')
+
+    with open(filename, 'w') as f:
+        f.write(data)
+
 if __name__ == '__main__':
     filename = '/root/showerthoughts.rss'
 
     remove_long_titles(filename)
+    check_for_duplicates('/root/shortshowerthoughts.rss')
 
-    logging.info('Finished removing long headlines from RSS feed.')
+    logging.info('Finished removing long headlines and duplicates from RSS feed.')
