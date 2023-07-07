0d1a37dc19552d7038e5faa51fd361483c0f067b
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-07 04:27:21 -0500

    remove_long_titles.py

--- remove_long_titles.py
+++ remove_long_titles.py
@@ -3,25 +3,25 @@ import os
 import logging
 
 def remove_long_titles(filename):
+    
 
     with open(filename, 'r') as f:
         data = f.read()
 
+
     title_regex = r'<title>(.*?)</title>'
     headlines = re.findall(title_regex, data)
 
-    short_headlines = []
+
     for headline in headlines:
-        if len(headline) <= 68 and headline.strip().lower() != "showerthoughts":
-            short_headlines.append(headline)
+        if len(headline) > 68 or headline.strip().lower() == "showerthoughts":
+            data = data.replace("<title>{}</title>".format(headline), '')
 
     with open(filename, 'w') as f:
         f.write(data)
 
     with open('/root/shortshowerthoughts.rss', 'a') as f:
-        for headline in short_headlines:
-            if headline not in f.read():
-                f.write('<title>{}</title>'.format(headline))
+        f.write(data)
 
 if __name__ == '__main__':
     filename = '/root/showerthoughts.rss'
