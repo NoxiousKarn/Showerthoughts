464e2002d6ef3403a11bff17511d1454aa111090
NoxiousKarn <119878111+NoxiousKarn@users.noreply.github.com>
2023-07-07 03:35:01 -0500

    remove_long_titles.py
    added code to output shortheadline to shortheadlines.rss in apend mode. meaning the list of short shower thoughts will grow as the pwnagotchi checks the RSS feed. I'm intending on modifying voice.py again to pull from shortshowerthoughts.rss vs showerthoughts.rss which gets overwritten. adding apend mode for showerthoughts.rss still caused the loss of changes made with this script when the RSS is downloaded. I'll let the script fill up shortST.rss for a few days before committing voice to call shortST.rss

--- remove_long_titles.py
+++ remove_long_titles.py
@@ -3,20 +3,21 @@ import os
 import logging
 
 def remove_long_titles(filename):
-    
+
     with open(filename, 'r') as f:
         data = f.read()
 
-    
     title_regex = r'<title>(.*?)</title>'
     headlines = re.findall(title_regex, data)
 
-    
     for headline in headlines:
         if len(headline) > 68 or headline.strip().lower() == "showerthoughts":
             data = data.replace("<title>{}</title>".format(headline), '')
 
     with open(filename, 'w') as f:
+        f.write(data)
+
+    with open('/root/shortshowerthoughts.rss', 'a') as f:
         f.write(data)
 
 if __name__ == '__main__':
@@ -25,3 +26,4 @@ if __name__ == '__main__':
     remove_long_titles(filename)
 
     logging.info('Finished removing long headlines from RSS feed.')
+
